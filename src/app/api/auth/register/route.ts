1ca7749c7e1294df2d9d82931d6b4b0377ee715f
Taha elhariri <tahaalhriri@gmail.com>
2025-02-22 00:36:14 +0300

    Initial commit for Next.js project

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
-import { supabase } from "../../../../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 
 
 export async function POST(req: NextRequest) {
   const { email, password } = await req.json();
 
-  const { user, error } = await supabase.auth.signUp({ email, password });
+  const { data: { user }, error } = await supabase.auth.signUp({ email, password });
 
   if (error) {
     return NextResponse.json({ error: error.message }, { status: 400 });
