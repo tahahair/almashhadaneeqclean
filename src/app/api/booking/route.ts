f58c41cd0f5a283beb998dbe587845efa80c9991
tahahair <taha.aihairtool@gmail.com>
2025-04-24 19:13:10 +0300

    Update route.ts

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -75,7 +75,7 @@ console.log('Received request:', {
     method: req.method,
     body: req.body,
     headers: req.headers,
-              NextResponse.json({ error: (error as Error).message }, { status: 400 })
+    NextResponse.json({ error: (error as Error).message }, { status: 400 })
   });
         
         return NextResponse.json({ error: (error as Error).message }, { status: 400 });
